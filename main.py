da9f9ff6c60aefba9cc6910b087a46385e03d925
Nero404 <jakub.nowak404@gmail.com>
2022-10-25 22:34:49 +0200

    Initial commit

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import IntegerManipulator
 
 # Wolałbym zrobić listę, ale nie wolno :<
 
-num1 = 110
-num2 = 270
-num3 = 330
+num_1 = 110
+num_2 = 270
+num_3 = 330
 
-myNumbers = IntegerManipulator.Manipulator(num1, num2, num3)
-print(f"Największym dzielnikiem liczb {num1}, {num2} i {num3} jest: {myNumbers.nwd()}")
+myNumbers = IntegerManipulator.Manipulator(num_1, num_2, num_3)
+print(f"Największym dzielnikiem liczb {num_1}, {num_2} i {num_3} jest: {myNumbers.nwd()}")
